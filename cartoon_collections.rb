bf17323b1b7556cc959982af312ee00d2fe1780f
Ji Yim <gsooyim@gmail.com>
2018-04-18 03:09:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(names)
   puts list
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
