aa18190a2d7b3b64ce5112eb141803d45459b496
Ji Yim <gsooyim@gmail.com>
2018-04-18 03:59:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,7 @@ def summon_captain_planet(planet_calls)
 end
 
 def long_planeteer_calls(calls)
-  state = false
-  calls.each do |call|
-    if call.length > 4
-      state = true
-    end
-  end
-  state
+  calls.any? (calls.length > 4)
 end
 
 def find_the_cheese(ingredients)
