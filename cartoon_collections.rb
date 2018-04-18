7a2773667fad79bc9e7bc0cbecd86ed6bd3e101a
Ji Yim <gsooyim@gmail.com>
2018-04-18 02:46:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names)
   names.each_with_index do |name, index|
     list += "#{index + 1}. #{name} \n"
   end
-  list
+  puts list
 end
 
 def summon_captain_planet# code an argument here
